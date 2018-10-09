c65e4b672dce85353af07cffa72e7f77c0fa9a0c
Hagen Paul Pfeifer <hagen@jauu.net>
2018-10-09 08:07:58 +0200

    rename to hippo2client
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
-setup(name='hippiagent',
+setup(name='hippo2client',
       version='1.3',
-      description='Client Adapter to HippiD',
+      description='Client Adapter to Hippo2D',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      url='http://github.com/hgn/hippiagent',
+      url='http://github.com/hgn/hippo2client',
       author='Hagen Paul Pfeifer',
       author_email='hagen@jauu.net',
       license='MIT',
-      packages=['hippiagent'],
+      packages=['hippo2client'],
       test_suite='nose.collector',
       tests_require=['nose'],
       classifiers=[
